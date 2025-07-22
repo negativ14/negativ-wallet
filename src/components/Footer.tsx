796349ce7e5d8542dbde7e5f01ca809752c4fc67
Rohit Mehta <rohitkumarmehta25@gmail.com>
2025-07-23 04:56:36 +0530

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,22 +9,25 @@ const Footer = () => {
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.4, ease: "easeInOut" }}
-      className="relative max-w-7xl mx-auto  px-4 flex justify-center items-center"
+      className="relative max-w-7xl mx-auto px-4 flex justify-center items-center h-20" // Ensure some height
     >
       {/* Fading top border line left/right */}
-      <div className="absolute top-0 left-0 w-full h-px bg-gradient-to-r from-transparent via-foreground/30 to-transparent" />
+      <div className="absolute top-0 left-0 w-full h-px bg-gradient-to-r from-transparent via-foreground/30 to-transparent z-0" />
 
-      <p className="text-foreground tracking-tight font-bold flex items-center gap-2 mt-5">
-        Connect on{" "}
-        <a
-          href="https://x.com/RohitMehta1409?t=n5-RF9x5Bx6anWj0BjSaSA&s=09"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center cursor-pointer"
-        >
-          <GradientXIcon className="size-4" />
-        </a>
-      </p>
+      {/* Text Content */}
+      <div className="relative z-10">
+        <p className="text-foreground tracking-tight font-bold flex items-center gap-2">
+          Connect on{" "}
+          <a
+            href="https://x.com/RohitMehta1409?t=n5-RF9x5Bx6anWj0BjSaSA&s=09"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center cursor-pointer"
+          >
+            <GradientXIcon className="size-4" />
+          </a>
+        </p>
+      </div>
     </motion.section>
   );
 };
