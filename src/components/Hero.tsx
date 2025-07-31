711e80ab7cba65ff642c052f6fe8f55017e69adc
Rohit Mehta <rohitkumarmehta25@gmail.com>
2025-07-31 17:44:13 +0530

    changes in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,18 +32,28 @@ const Hero = ({ generateSecretPhrase }: HeroProps) => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.4, ease: "easeInOut" }}
       >
-        <h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold tracking-tighter leading-tight break-keep">
+        {/* <h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold tracking-tighter leading-tight break-keep">
           Generate&nbsp;or&nbsp;
           <span className="bg-gradient-to-b from-blue-400 to-blue-600 bg-clip-text text-transparent (0px,0px,20px,.20) ">
             Access&nbsp;Crypto
           </span>
           <br />
           <span className="block">Wallets Instantly</span>
+        </h1> */}
+
+        <h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold tracking-tighter leading-snug text-center break-words px-2">
+          Generate or
+          <br className="sm:hidden" /> {/* Break only on phones */}
+          <span className="bg-gradient-to-b from-blue-400 to-blue-600 bg-clip-text text-transparent drop-shadow-md">
+            Access Crypto
+          </span>
+          <br />
+          <span className="block mt-2">Wallets Instantly</span>
         </h1>
 
         <p className="text-sm sm:text-md lg:text-lg text-foreground/80 max-w-xl font-semibold -mt-4">
           Secure key generation from your seed phrase.{" "}
-          <span className="block sm:inline text-center">
+          <span className="hidden sm:inline text-center">
             Nothing is stored.
           </span>
         </p>
